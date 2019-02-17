86fa0f3f4961c93b4be157e4b77dfb5ad73a9256
apa4iqr <apachi777@gmail.com>
2019-02-17 22:27:00 +0200

    Add files via upload

---
+++ ReadApp1/ScreenLibary.swift
@@ -0,0 +1,16 @@
+//
+//  ReadScreen.swift
+//  ReadApp1
+//
+//  Created by s.pogotovka on 17.02.2019.
+//  Copyright © 2019 ApachiGroup. All rights reserved.
+//
+
+
+import UIKit
+
+class  Libary : UIViewController {
+    
+    
+    
+}
---
+++ ReadApp1/ScreenRead.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ReadApp1
+//
+//  Created by s.pogotovka on 17.02.2019.
+//  Copyright © 2019 ApachiGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var LibaryButton: UIButton!
+    @IBOutlet weak var SettingButton: UIButton!
+    
+    
+    @IBOutlet weak var ReadScreen: UIScrollView!
+    @IBOutlet weak var PlayTextButton: UIButton!
+    @IBOutlet weak var UpSpeedButton: UIButton!
+    @IBOutlet weak var DownSpeedButton: UIButton!
+    @IBOutlet weak var UpSizeButton: UIButton!
+    @IBOutlet weak var DownSizeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
